3925b885b98744ffb2840ed08e8b69f9ed96bc01
RemiEfrei <remi.griot@efrei.fr>
2022-02-08 21:29:52 +0100

    aa

--- 05_CubeIDE/1DOF_Bench/Core/Src/remi.c
+++ 05_CubeIDE/1DOF_Bench/Core/Src/remi.c
@@ -52,3 +52,17 @@ void sendbackRS232(UART_HandleTypeDef *huart) {
 	}
 }
 
+// Redefine _write function for printf
+int _write(int file, char *data, int len) {
+	if ((file != STDOUT_FILENO) && (file != STDERR_FILENO)) {
+		errno = EBADF;
+		return -1;
+	}
+
+	// arbitrary timeout 1000
+	HAL_StatusTypeDef status = HAL_UART_Transmit(&huart2, (uint8_t*) data, len,
+			1000);
+
+	// return # of bytes written - as best we can tell
+	return (status == HAL_OK ? len : 0);
+}
