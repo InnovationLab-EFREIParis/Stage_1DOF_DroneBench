6da2472f4f3506b993b31dd17449e328282d8679
unknown <yannaxel18@gmail.com>
2022-03-21 17:10:39 +0100

    clean clean code v1

--- 05_CubeIDE/1DOF_Bench/Core/Src/main.c
+++ 05_CubeIDE/1DOF_Bench/Core/Src/main.c
@@ -70,7 +70,19 @@ static void MX_TIM3_Init(void);
 
 uint8_t gaz_buffer[4];
 
+int _write(int file, char *data, int len) {
+	if ((file != STDOUT_FILENO) && (file != STDERR_FILENO)) {
+		errno = EBADF;
+		return -1;
+	}
 
+	// arbitrary timeout 1000
+	HAL_StatusTypeDef status = HAL_UART_Transmit(&huart2, (uint8_t*) data, len,
+			1000);
+
+	// return # of bytes written - as best we can tell
+	return (status == HAL_OK ? len : 0);
+}
 
 
 
